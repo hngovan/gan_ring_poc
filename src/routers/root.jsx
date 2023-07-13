14dd45821ff8ad907867fdb534a26f67ea879d66
hngovan <hngovan1999@gmail.com>
2023-07-13 17:38:48 +0700

    fix bug router

--- src/routers/root.jsx
+++ src/routers/root.jsx
@@ -1,24 +1,23 @@
-import { createBrowserRouter, Route  } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import ErrorPage from "../error-page";
 import Login from "../components/login.jsx";
 import Home from "../components";
 import PrivateRoutes from "../utils/PrivateRoutes";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <PrivateRoutes>
-        <Route path="/" element={<Home />} />
-      </PrivateRoutes>
-    ),
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route element={<PrivateRoutes />}>
+        <Route path="/" element={<Home />} errorElement={<ErrorPage />} exact />
+      </Route>
+      <Route path="/login" element={<Login />} errorElement={<ErrorPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </>
+  )
+);
 
 export default router;
