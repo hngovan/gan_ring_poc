cbc1dcb709692967fc42572bf88e74db3dac359b
hngovan <hngovan1999@gmail.com>
2023-07-13 11:38:37 +0700

    private router

--- src/routers/root.jsx
+++ src/routers/root.jsx
@@ -1,18 +1,23 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import ErrorPage from "../error-page";
 import Login from "../components/login.jsx";
 import Home from "../components";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />,
-  },
-]);
+import PrivateRoutes from "../utils/PrivateRoutes";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route element={<PrivateRoutes />}>
+        <Route path="/" element={<Home />} errorElement={<ErrorPage />} exact />
+      </Route>
+      <Route path="/login" element={<Login />} errorElement={<ErrorPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </>
+  )
+);
 
 export default router;
